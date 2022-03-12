01946e7ab62a9d5095a41d93eb144ab278db1337
julioflores117 <julo_1320114015@uptecamac.edu.mx>
2022-03-11 18:39:44 -0600

    hola

---
+++ database/migrations/2022_03_11_023829_ubicaciones.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ubications', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('codigo');
+            $table->string('asentamiento');
+            $table->string('tipo_asenta');
+            $table->string('municipio');
+            $table->string('estado');
+           
+            $table->string('ciudad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/seeders/ubicaciones.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ubicaciones extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $acentamiento=['Aguascalientes Centro','Colinas del Rio','Olivares Santana','Las Brisas','San Cayetano','Colinas de San Ignacio','La Fundición'];
+        $municipio=['acolman','tecamac','teotihuacan'];
+        $estado=['Aguascalientes','acapulco','la paz'];
+        for($i=0;$i<134061;$i++){
+            
+        }
+    }
+}
